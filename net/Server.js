0ed7f52e0784cbe1c9dbd0ebd64335e4393a9f90
Robin Stamer <RobinStamer@users.noreply.github.com>
2022-07-04 08:31:38 -0600

    Enable basic dialstring support for net/Server (#1)
    Co-authored-by: R <R@nobl>

--- net/Server.js
+++ net/Server.js
@@ -2,6 +2,9 @@ const MessageService = require('../protocol/MessageService').MessageService;
 
 const net = require('net');
 
+// Check for tcp!host!port or unix!socket dialstrings
+const rDial = /^(tcp![^!]+![0-9]+|unix![^!]+)$/;
+
 const server = net.createServer(c => {
 	c.on('end', () => console.log('server disconnected!'));
 	c.on('data', blob => MessageService.parse(blob).forEach(message => {
@@ -11,6 +14,20 @@ const server = net.createServer(c => {
 		}
 	}));
 });
+
+server.listen = (listen => {
+	return function (...args)
+	{
+		if (rDial.test(args[0]))
+		{
+			listen.apply(this, args[0].split('!').slice(1).concat(args.slice(1)));
+		}
+		else
+		{
+			listen.apply(this, args);
+		}
+	}
+})(server.listen);
 
 module.exports = { Server:server };
 
